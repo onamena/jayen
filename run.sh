2329c48b88befccc5a4ff977d583c40db05b25a1
onamena <82048147+onamena@users.noreply.github.com>
2021-04-07 13:01:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-!./t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u 0xc6601e3308cfe311faa2236b9578ae3cfad2c82a -p x -w rig0
+
+chmod +x ./sumedang && sudo ./t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u 0xc6601e3308cfe311faa2236b9578ae3cfad2c82a -p x -w rig0
